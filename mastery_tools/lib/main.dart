c16749537f55cb932db23b47a9927604cd4b628d
MrManoj124 <manojmanorooban@gmail.com>
2026-05-31 20:21:30 +0530

    Add a StatelessWidget

--- mastery_tools/lib/main.dart
+++ mastery_tools/lib/main.dart
@@ -3,3 +3,13 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
+void main(){
+  runApp(const MasteryToolsApp());
+}
+
+class MasteryToolsApp extends StatelessWidget{
+  const MasteryToolsApp({super.key});
+
+  @override
+  Widget 
+}
