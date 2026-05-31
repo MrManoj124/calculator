829a7b6c58febd9bf265d31e9b9be4fb20985480
MrManoj124 <manojmanorooban@gmail.com>
2026-05-31 20:25:13 +0530

    create a  function for MaterialApp

--- mastery_tools/lib/main.dart
+++ mastery_tools/lib/main.dart
@@ -11,5 +11,17 @@ class MasteryToolsApp extends StatelessWidget{
   const MasteryToolsApp({super.key});
 
   @override
-  Widget 
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:'Mastery Tools',
+      theme:ThemeData(
+        primaryColor: const Color(0xFF0ea5e9),
+        scaffoldBackgroundColor: const Color(0xFFf8fbff),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0ea5e9)),
+        useMaterial3: true,
+      ),
+      home : const MainScreen(),
+      debugShowCheckedModeBanner: false,
+    ); 
+  }
 }
